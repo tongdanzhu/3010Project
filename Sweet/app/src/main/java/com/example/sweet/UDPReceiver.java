8762fd3af2800b49cb0f12fe20bd33eeb2a1c2a9
Lixuan Luo <lixuanluo@icloud.com>
2019-11-23 20:35:54 -0500

    update

--- Sweet/app/src/main/java/com/example/sweet/UDPReceiver.java
+++ Sweet/app/src/main/java/com/example/sweet/UDPReceiver.java
@@ -4,44 +4,35 @@ import java.net.*;
 
 public class UDPReceiver {
 
-    private final static int PACKETSIZE = 100 ;
+    private final static int PACKETSIZE = 1000 ;
 
     public static void main( String args[] )
     {
-        // Check the arguments
-        /*if( args.length != 1 )
-        {
-            System.out.println( "usage: UDPReceiver port" ) ;
-            return ;
-        }*/
+        //System.out.println( "Receiving on port " ) ;
         try
         {
-            // Convert the argument to ensure that is it valid
-            int port = 8685;
-                 //   Integer.parseInt( args[0] ) ;
+            int port = 8688;
 
             // Construct the socket
             DatagramSocket socket = new DatagramSocket( port ) ;
 
-            //System.out.println( "usage: UDPReceiver port" ) ;
-
+            for(;;)
             {
                 System.out.println( "Receiving on port " + port ) ;
                 DatagramPacket packet = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
-                System.out.println( "134343434" ) ;
-
                 socket.receive( packet ) ;
-                //System.out.println( "234343434" ) ;
-                String message = "ACK: " + new String(packet.getData()).trim();
-                //System.out.println( "34343434" ) ;
+
+               // String message = "ACK: " + new String(packet.getData()).trim();
+                String message = "20";
+
                 byte [] data = message.getBytes();
-                //System.out.println( "434343434" ) ;
-                DatagramPacket sePacket = new DatagramPacket( data, 20, packet.getAddress(), packet.getPort() ) ;
-                //System.out.println( "534343434" ) ;
+
+                DatagramPacket sePacket = new DatagramPacket( data, data.length, packet.getAddress(), packet.getPort() ) ;
+
                 socket.send( sePacket) ;
-                //System.out.println( "634343434" ) ;
+
                 System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim() ) ;
-                //System.out.println( "734343434" ) ;
+
             }
         }
         catch( Exception e )
