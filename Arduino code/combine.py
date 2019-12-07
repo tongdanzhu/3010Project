30781a05e02cf4d99dbdeeade2c95e62902fb908
XuchengShi0 <55570235+XuchengShi0@users.noreply.github.com>
2019-12-07 00:07:00 -0500

    Update combine.py

--- Arduino code/combine.py
+++ Arduino code/combine.py
@@ -1,13 +1,10 @@
 import serial
-
 import time
-
 import MySQLdb as mdb
-
 import sys
-
 import RPi.GPIO as GPIO
 
+#setup GPIO PIN
 channel = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -16,42 +13,31 @@ GPIO.setup(25,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
 GPIO.setup(channel,GPIO.OUT)
 close = True
-
 nomail = True
 
-
+#connect to Arduino
 arduino = serial.Serial("/dev/ttyACM0")
-
 arduino.baudrate=9600
-
 time.sleep(2)
 
+#connect to MYSQL database
 con = mdb.connect('localhost','root','password','sysc');
 
+# read information from Arduino
 while True:
-
  data = arduino.readline()
-
  time.sleep(2)
-
  data = arduino.readline()
-
  pieces =data.split("\t")
-
  intensity = pieces[0]
-
  lightstate = pieces[1]
-
  mail = pieces[2]
-
  temp = pieces[4]
-
  bell = pieces[5]
  print(data)
 
-
+#information handling
  with con:
-
    cur =con.cursor()
    cur.execute("INSERT INTO light(houseID, currTime, currDate, intensity) VALUES(1, CURTIME(), CURDATE(), '%s')"%(intensity))
    cur.execute("INSERT INTO temperature(houseID, currTemp, currDate, currTime) VALUES(1, '%s', CURDATE(), CURTIME())"%(temp))
@@ -60,20 +46,24 @@ while True:
    new_dict = dict(zip([x[0] for x in cur.description],t))
    print(t[0])
    print(temp)
+   
    if int(temp)>int(t[0]):
       GPIO.output(channel, False)
       cur.execute("UPDATE temperatureControl SET fanState = 1  WHERE houseID = 1")
    if int(temp)<int(t[0]):
       GPIO.output(channel, True)
       cur.execute("UPDATE temperatureControl SET fanState = 0  WHERE houseID = 1") 
+      
    cur.execute("SELECT mailboxstate FROM user")
    b = cur.fetchone()
    b_dict = dict(zip([x[0] for x in cur.description],b))
    if b[0]==0:
      if int(mail)>0:
        cur.execute("UPDATE user SET mailboxstate = 1  WHERE houseID = 1")
+       
    if int(bell)==1:
       cur.execute("INSERT INTO visitor(houseID, currTime, currDate, confirm) VALUES(1, CURTIME(), CURDATE(), 0)")
+     
    cur.execute("SELECT manualControl FROM lightControl")
    m = cur.fetchone()
    m_dict = dict(zip([x[0] for x in cur.description],m))
@@ -99,6 +89,7 @@ while True:
        GPIO.output(24,True)
        GPIO.output(25,True)
        GPIO.output(12,True)
+       
    print(g)
   
    
